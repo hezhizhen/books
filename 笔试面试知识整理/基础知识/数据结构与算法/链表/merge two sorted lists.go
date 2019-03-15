fa750ec02b90210bc27b552bd8d9fc62919c3fb0
hezhizhen <hezhizhen.yi@gmail.com>
2019-03-15 17:19:17 +0800

    Merge Two Sorted Lists
    LeetCode 21

---
+++ 笔试面试知识整理/基础知识/数据结构与算法/链表/merge two sorted lists.go
@@ -0,0 +1,47 @@
+package main
+
+/*
+// Recursive
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+    if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val{
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	}
+	l2.Next = mergeTwoLists(l1, l2.Next)
+	return l2
+}
+*/
+
+// Iterative
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	head := &ListNode{}
+	curr := head
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			curr.Next = l1
+			l1 = l1.Next
+		} else {
+			curr.Next = l2
+			l2 = l2.Next
+		}
+		curr = curr.Next
+	}
+	if l1 != nil {
+		curr.Next = l1
+	} else {
+		curr.Next = l2
+	}
+	return head.Next
+}
